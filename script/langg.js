5c5dc80fc077de75f4b7657bf2928462a03e43ff
BarinovVlad <vladislav.barinovv@gmail.com>
2024-08-19 18:20:29 +0200

    f

---
+++ script/langg.js
@@ -0,0 +1,30 @@
+// const langArr = {
+//     "title_wrapp" :  {
+//         "en": "Build stunning websites & apps.",
+//         "ru": "создаем многоязычный сайт",
+//         "ua": "створюємо багатомовний сайт",
+//     }, 
+//     "title_media" :  {
+//         "en": "Build stunning websites & apps.",
+//         "ru": "создаем многоязычный сайт",
+//         "ua": "створюємо багатомовний сайт",
+//     }, 
+//     "header-text" :  {
+//         "en": " Create live segments and target the right people for messages based on their behaviors.",
+//         "ru": "Сделай это и будет все круто",
+//         "ua": "графічний чіп",
+//     },
+//     "icons-bold" :  {
+//         "en": " What is it about?",
+//         "ru": "О чем это?",
+//         "ua": "Про що йдеться?",
+//     },
+   
+// }
+
+const langArr = {
+    "header-text" :  {
+        "en": "Create live segments and target the right people for messages based on their behaviors.",
+        "ua": "створюємо багатомовний сайт",
+    }, 
+}
